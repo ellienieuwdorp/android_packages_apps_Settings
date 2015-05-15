ellienieuwdorp/android_packages_apps_Settings

commit c45cafadbb24a579ae0cdec76cf48683d6b9f5ef

Settings: disable mobile network switch when SIM isn't ready
Change-Id: I4676390760f0d6107e706fe75cd26f7bf603cad6
Signed-off-by: Roman Birg <roman@cyngn.com>

src/com/android/settings/dashboard/MobileNetworksEnabler.java
@@ -83,8 +83,10 @@ public class MobileNetworksEnabler extends GenericSwitchToggle  {
     }
 
     private void updateState() {
-        setEnabled(mTelephonyManager.getDataState() != TelephonyManager.DATA_UNKNOWN);
-        setChecked(mTelephonyManager.getDataEnabled());
+        final boolean simReady = mTelephonyManager.getSimState()
+                == TelephonyManager.SIM_STATE_READY;
+        setEnabled(simReady && mTelephonyManager.getDataState() != TelephonyManager.DATA_UNKNOWN);
+        setChecked(simReady && mTelephonyManager.getDataEnabled());
     }
 
     @Override

commit a49393c6a4845cd85390ca1dfcd990840e02126f

Fix mobile network switch for CDMA phones
commit c45cafadbb24a579ae0cdec76cf48683d6b9f5ef
inadvertently removed the ability to enable/disable mobile
network on CDMA phones.

It currently is always enabled.

Change-Id: I70a1b5fd63c49842f281823e6cd59a391d05934b

src/com/android/settings/dashboard/MobileNetworksEnabler.java
@@ -83,8 +83,8 @@ public class MobileNetworksEnabler extends GenericSwitchToggle  {
     }
 
     private void updateState() {
-        final boolean simReady = mTelephonyManager.getSimState()
-                == TelephonyManager.SIM_STATE_READY;
+        final boolean simReady = mTelephonyManager.getSimState() == TelephonyManager.SIM_STATE_READY
+                || mTelephonyManager.getPhoneType() == TelephonyManager.PHONE_TYPE_CDMA;
         setEnabled(simReady && mTelephonyManager.getDataState() != TelephonyManager.DATA_UNKNOWN);
         setChecked(simReady && mTelephonyManager.getDataEnabled());
     }